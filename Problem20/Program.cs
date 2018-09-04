b7fa5dd84c303531e13c2137a672f0a9b43158b5
Michal Tichý <michal.tichy@riganti.cz>
2018-09-04 20:58:15 +0200

    Add project files.

---
+++ Problem20/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+namespace Problem20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bigInteger = new BigInteger(100);
+            for (int i = 99; i != 0; i--)
+            {
+                bigInteger *= i;
+            }
+
+            var result = GetSumOfCharacters(bigInteger.ToString());
+            Console.Out.WriteLine("result = {0}", result);
+            Console.ReadKey();
+        }
+
+        private static int GetSumOfCharacters(string input)
+        {
+            return input.ToString().Sum(t => t - '0');
+        }
+    }
+}
